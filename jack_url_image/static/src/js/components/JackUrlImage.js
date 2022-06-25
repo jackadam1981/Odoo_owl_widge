df595427e9c7177d00961879932a568f6003e0d7
jack <jackadam@sina.com>
2022-06-25 19:58:34 +0800

    5.覆盖表单渲染器以将组件安装到类

--- jack_url_image/static/src/js/components/JackUrlImage.js
+++ jack_url_image/static/src/js/components/JackUrlImage.js
@@ -1,13 +1,26 @@
 // code:myaddons/jack_url_image/static/src/js/components/JackUrlImage.js
 odoo.define("jack_url_image.JackUrlImage", function (require) {
+
+    const FormRenderer = require("web.FormRenderer");
     const {Component} = owl;
+    const {ComponentWrapper} = require("web.OwlCompatibility");
 
     class JackUrlImage extends Component {
         //
-    };
+    }
 
     Object.assign(JackUrlImage, {           //新加内容
         template: "jack_url_image.JackUrlImage"    // 新加内容
     });                                            // 新加内容
 
+    FormRenderer.include({
+        async _render() {
+            await this._super(...arguments);
+
+            for (const element of this.el.querySelectorAll(".o_jack_url_image")) {
+                (new ComponentWrapper(this, JackUrlImage))
+                    .mount(element)
+            }
+        }
+    });
 });
